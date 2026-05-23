86ecb6471b86b312911756a08d32e45bd00c2fa0
Jeffergs <jeffersongs1995@gmail.com>
2026-05-23 16:25:07 -0300

    Cria componente de responsabilidades dos integrantes

---
+++ src/data/time.js
@@ -0,0 +1,19 @@
+const timeData = [
+  {
+    name: "Jefferson Gomes",
+    role: "Pesquisa Científica",
+    description:
+      "Responsável pela pesquisa científica, validação médica e organização dos dados de saúde.",
+    image: "/images/jefferson.jpg"
+  },
+
+  {
+    name: "Natalia",
+    role: "Frontend UI/UX",
+    description:
+      "Responsável pela construção visual e experiência do usuário.",
+    image: "/images/natalia.jpg"
+  }
+];
+
+export default teamData;
