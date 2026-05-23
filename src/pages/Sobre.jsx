84e22e7b29c72618a569cbd7ed00abe06ecfcf45
Jeffergs <jeffersongs1995@gmail.com>
2026-05-23 18:33:58 -0300

    adiciona component Time na página Sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,8 +1,10 @@
 import StacksUsadas from "../components/sobreProjeto/StacksUsadas"
+import Time from "../components/sobreProjeto/Time"
 
 export default function Layout() {
     return (
         <>
+          <Time />  
           <StacksUsadas />
         </>
     )
