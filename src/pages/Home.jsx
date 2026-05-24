42bffa4916733d77d46a0f80f0e8fdbe7f1b68ae
nalugao <nalugao@hotmail.com>
2026-05-24 20:22:36 -0300

    feat: add Inicio, MapaCorporal, Painel, and Simulador components with styles; update Home page imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import Inicio from '../sections/inicio/Inicio'
-import Painel from '../sections/painel/Painel'
-import MapaCorporal from '../sections/mapaCorporal/MapaCorporal'
-import Simulador from '../sections/simulador/Simulador'
+import Inicio from '../components/inicio/Inicio'
+import Painel from '../components/painel/Painel'
+import MapaCorporal from '../components/mapaCorporal/MapaCorporal'
+import Simulador from '../components/simulador/Simulador'
 
 export default function Home() {
     return (
